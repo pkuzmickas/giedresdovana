74df39f4777564524f1b0ab63be1e8edbac3941d
Paulius Kuzmickas <pkuzmickas@spotify.com>
2024-12-25 22:15:23 +0000

    pics

--- src/components/UnlockedPage/UnlockedPage.tsx
+++ src/components/UnlockedPage/UnlockedPage.tsx
@@ -25,21 +25,62 @@ export const UnlockedPage = () => {
         }}
         onAnimationEnd={() => setIsBouncing(false)}
         style={{ animationIterationCount: 1 }}
+        width="420vw"
       />
     </div>
   );
 
-  return intro ? <img src="dance.gif" /> : !pics ? ChristmasTree : (
-    <div className="text-4xl font-semibold text-white flex flex-col gap-8">
-        <div>Giedre 2024</div>
-        <div>Giedre 2023</div>
-        <div>Giedre 2022</div>
-        <div>Giedre 2021</div>
-        <div>Giedre 2020</div>
-        <div>Giedre 2019</div>
-        <div>Giedre 2018</div>
-        <div>Giedre 2017</div>
-        <div>Giedre 2016</div>
+  return intro ? (
+    <img src="dance.gif" />
+  ) : !pics ? (
+    ChristmasTree
+  ) : (
+    <div className="text-3xl font-semibold text-white flex flex-col gap-8 items-center">
+      <div className="w-full h-2 bg-white" />
+      <div>Ačiū už draugystę.</div>
+      <div>Šį nuotraukų albumą vadinu:</div>
+      <div className="font-bold italic">"P & G over the years"</div>
+      <div className="w-full h-2 bg-white" />
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2016</div>
+        <img src="2016.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2017</div>
+        <img src="2017.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2018</div>
+        <div className="text-5xl font-bold">IEVA</div>
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2019</div>
+        <img src="2019.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2020</div>
+        <img src="2020.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2021</div>
+        <img src="2021.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2022</div>
+        <img src="2022.jpg" width="100%" />
+      </div>
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2023</div>
+        <img src="2023.jpg" width="100%" />
+      </div>
+      <div>⭐⭐⭐🎄🎄🎄🎄🎄🎄⭐⭐⭐</div>
+      <div className="w-full h-2 bg-white" />
+      <div className="md:p-48 p-8 flex flex-col items-center gap-8 pt-4">
+        <div>Aš ir Giedrė 2024</div>
+        <img src="2024.jpg" width="100%" />
+      </div>
+      <div className="w-full h-2 bg-white" />
+      <div>⭐⭐⭐🎄🎄🎄🎄🎄🎄⭐⭐⭐</div>
     </div>
   );
 };
